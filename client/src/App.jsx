6a5576c15e70d4f8be4b7376d8046664df677d6a
sayank.khutia123@gmail.com <sayank.khutia123@gmail.com>
2024-12-23 23:49:45 +0530

    Project Setup (Client)

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Doctors from './pages/Doctors'
+import MyProfile from './pages/MyProfile'
+import MyAppointments from './pages/MyAppointments'
+import Login from './pages/Login'
+import Appointment from './pages/Appointment'
+const App = () => {
+  return (
+    <div className="mx-4 sm:mx-[10%]">
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:speciality' element={<Doctors />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/my-appointments' element={<MyAppointments />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
